aa70733b3742195d7a013fbf840a93e13b99eadf
Fanmardock <francesco.gaeta90@gmail.com>
2026-08-26 09:03:11 +0200

    using base image

--- build_files/scripts/20-packages.sh
+++ build_files/scripts/20-packages.sh
@@ -50,7 +50,7 @@ rum install -y --allowerasing \
     code \
     libvirt virt-manager qemu-kvm flatpak-builder wlr-randr \
     iotop sysstat lxqt-openssh-askpass lxpolkit parallel \
-    cosmic-store \
+    rakuos-software-gtk \
     akmod-xpadneo \
     nautilus \
     kitty \
@@ -64,8 +64,3 @@ rum install -y --allowerasing \
     gnome-disk-utility \
     nautilus-open-any-terminal
 
-## ==========================================
-## 4. RIMOZIONE COMPONENTI RIDONDANTI
-## ==========================================
-# Rimozione di componenti COSMIC e launcher inutilizzati
-rum remove waybar swaylock alacritty cosmic-comp cosmic-initial-setup cosmic-settings || true
